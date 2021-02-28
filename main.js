a00a7ed5a20b1c982ff51a50d816249c53c20d0b
sn00wden <matthieu2603@gmail.com>
2021-02-28 23:07:43 +0100

    Add début Reaction role

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = '-';
+const id_channel = "815640341495021638";
+const id_message = "815678486136094763";
+
+
+//id role is
+const hanyang_role = "813712419007234090" ;
+const inha_role = "813712674771697665" ;
+const chungang_role = "813712613346902068";
+const skku_role = "813713401641566238";
+const seoultech_role = "813713484793511967";
+const kumoh_role = "813713287484932106";
 
 const fs = require('fs');
 
@@ -24,24 +35,66 @@ client.once('end',() =>{
 });
 
 client.on('ready',() =>{
-    client.guilds.cache.find(guild =>guild.id === "813711622010830868").channels.cache.find(channel => channel.id === "813722716175335474").messages.fetch("813724982130507816").then(message =>{
+    client.guilds.cache.find(guild =>guild.id === "813711622010830868").channels.cache.find(channel => channel.id === id_channel).messages.fetch(id_message).then(message =>{
         console.log("message choix université ajouté");
     }).catch(err =>{
-        console.log("ERROR");
+        console.log("ERROR" + err);
     })
 })
 
 client.on('messageReactionAdd',(reaction,user) =>{
     console.log("Reaction ajouté");
-    if(reaction.message.id === "813724982130507816"){
-        if(reaction.emoji.name === "one"){
-           var member = reaction.message.guild.member.cache.find(member => member.id === user.id);
-           member.roles.add("813712419007234090").then(mbr => {
+    if(reaction.message.id === id_message){
+        if(reaction.emoji.name === '😂'){ //Hanyang
+           var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+           member.roles.add(hanyang_role);
+           /*member.roles.add(hanyang_role).then(mbr => {
                console.log("Role attribué avec succès pour" + mbr.displayName);
            }).catch(() => {
                console.log("Role pas attribué");
-           });
-        } 
+           });*/
+        } else if(reaction.emoji.name === "two"){ //Inha
+            var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+            member.roles.add(inha_role).then(mbr => {
+                console.log("Role attribué avec succès pour" + mbr.displayName);
+            }).catch(() => {
+                console.log("Role pas attribué");
+            });
+         }else if(reaction.emoji.name === "three"){ //Chung-Ang
+            var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+            member.roles.add(chungang_role).then(mbr => {
+                console.log("Role attribué avec succès pour" + mbr.displayName);
+            }).catch(() => {
+                console.log("Role pas attribué");
+            }); 
+        }
+        else if(reaction.emoji.name === "four"){ //SKKU
+            var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+            member.roles.add(skku_role).then(mbr => {
+                console.log("Role attribué avec succès pour" + mbr.displayName);
+            }).catch(() => {
+                console.log("Role pas attribué");
+            }); 
+        }else if(reaction.emoji.name === "five"){ //Seoultech
+            var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+            member.roles.add(seoultech_role).then(mbr => {
+                console.log("Role attribué avec succès pour" + mbr.displayName);
+            }).catch(() => {
+                console.log("Role pas attribué");
+            }); 
+        }else if(reaction.emoji.name === "six"){ //Kumoh
+            var member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+            member.roles.add(kumoh_role).then(mbr => {
+                console.log("Role attribué avec succès pour" + mbr.displayName);
+            }).catch(() => {
+                console.log("Role pas attribué");
+            }); 
+        }
+
+            if(!user.bot){
+            reaction.users.remove(user.id);}
+            
+        
     }
 })
 
@@ -64,4 +117,4 @@ client.on('message', message =>{
     }
 });
 
-client.login('ODE1NjM3NDA3MTkzMTA0Mzk0.YDvThw.n4JU9zD0S77Hhc4OQ9dXxdeO6lA');
+client.login('ODE1NjM3NDA3MTkzMTA0Mzk0.YDvThw.8qTcgsDhxMP5cmCTh0xeLrKAyf8');
